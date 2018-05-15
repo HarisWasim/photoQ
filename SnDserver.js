ae5f19d0848127565cd0767199542b1d62ecc447
Jose Alvarado-Torre <jos.alvatorre@gmail.com>
2018-05-15 16:35:16 -0700

    moved files out of public (deleted one), and changed to Jose's port number

--- SnDserver.js
+++ SnDserver.js
@@ -3,7 +3,7 @@ var static = require('node-static');
 var fs = require('fs');
 
 var file = new static.Server('./');
-var port = 57641;
+var port = 50232;
 //START SECTION import json
 var fs = require('fs');// file access module
 var photoURLArray = [];
